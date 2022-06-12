67595effa66c739e6875f9969cf5712248c69c3e
IK. Surya Negara <suryastupid.gila@gmail.com>
2022-06-12 22:48:19 +0800

    tugas Challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
-
+const fs = require('fs/promises')
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const file = './external.js'
+const utf8 = 'utf-8'
+
+const promiseOutput = async (args) => {
+  
+  try {
+    let result= [];
+    const hasil_ixx = await promiseTheaterIXX(fs.readFile(file, utf8))
+    const hasil_vgc = await promiseTheaterVGC(fs.readFile(file, utf8))
+    result.push(hasil_ixx)
+    result.push(hasil_vgc)
+
+    if (args === 'marah') {
+      const ixxCount = result[0].filter((a) => a.hasil == 'marah')
+      const vgcCount = result[1].filter((a) => a.hasil == 'marah')
+      return ixxCount.length + vgcCount.length
+    }
+    else {
+      const vgcCount = result[0].filter((a) => a.hasil == 'tidak marah')
+      const vgcCount = result[1].filter((a) => a.hasil == 'tidak marah')
+      return vgcCount.length + vgcCount.length
+    }
+  }
+  catch(err) {
+    return err
+  }
+}
 
 module.exports = {
   promiseOutput,
